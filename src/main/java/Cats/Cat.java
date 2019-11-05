053b52fef24c9c3b120cc587972c6590e572408a
Mihan666 <dombai91@mail.ru>
2019-11-05 23:03:18 +0300

    Add cat lastName

--- src/main/java/Cats/Cat.java
+++ src/main/java/Cats/Cat.java
@@ -5,11 +5,11 @@ public class Cat {
     private int age = 4;
 
     private String name = "Barsik";
-
+    private String lastName = "Котиков";
 
 
     @Override
     public String toString() {
-        return "коту "+ age+ ", лет";
+        return "коту " + age + ", лет";
     }
 }
